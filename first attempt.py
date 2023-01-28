6dc3e3550e24b04b70a1e75c8fc6cae53a983ce0
human <43803772+0xhuman@users.noreply.github.com>
2023-01-27 21:16:31 -0800

    fixed overlay crop issue + alignment
    Signed-off-by: human <43803772+0xhuman@users.noreply.github.com>

--- first attempt.py
+++ first attempt.py
@@ -5,40 +5,42 @@ from io import BytesIO
 # function to position images
 def position_images(urls):
     # create a new blank image
-    #new_image = Image.new('RGB', (1000, 1000), color = (255, 255, 255))
-    new_image = Image.new('RGBA', (1000, 1000), color = (255, 255, 255, 0))
+    new_image = Image.new('RGBA', (1000, 1000), color = (0, 0, 0, 0))
     images = []
     # open and resize images
     for url in urls:
-        image_bytes = urllib.request.urlopen(url).read()
-        image = Image.open(BytesIO(image_bytes))
-        image = image.resize((500, 500))
+        image = Image.open(BytesIO(urllib.request.urlopen(url).read()))
+
+        image = image.convert('RGBA').resize((500, 500))
         images.append(image)
 
     if len(images) == 1:
         # paste the image in the middle of the new image
-        new_image.paste(images[0], (250, 250))
+        images = []
+        image = image.convert('RGBA').resize((1000, 1000))
+        images.append(image)
+        new_image.paste(images[0], (0, 0))
     elif len(images) == 2:
-        # paste the first image on the left side
-        new_image.paste(images[0], (0, 250))
-        # paste the second image on the right side
-        new_image.paste(images[1], (500, 250))
+        # bottom right
+        new_image.alpha_composite(images[0], (350, 350))
+        # bottom left
+        new_image.alpha_composite(images[1], (0, 350))
     elif len(images) == 3:
-        # paste the first image on the top
-        new_image.paste(images[0], (0, 0))
-        # paste the second image on the top right
-        new_image.paste(images[1], (500, 0))
-        # paste the third image on the bottom
-        new_image.paste(images[2], (250, 500))
+        # bottom right
+        new_image.alpha_composite(images[0], (350, 350))
+        # bottom left
+        new_image.alpha_composite(images[1], (0, 350))
+        # top middle
+        new_image.alpha_composite(images[2], (175, 0))
     elif len(images) == 4:
-        # paste the first image on the top left
-        new_image.paste(images[0], (0, 0))
-        # paste the second image on the top right
-        new_image.paste(images[1], (500, 0))
-        # paste the third image on the bottom left
-        new_image.paste(images[2], (0, 500))
-        # paste the fourth image on the bottom right
-        new_image.paste(images[3], (500, 500))
+        # bottom right
+        new_image.alpha_composite(images[0], (350, 350))
+        # bottom left
+        new_image.alpha_composite(images[1], (0, 350))
+        # bottom right
+        new_image.alpha_composite(images[2], (350, 0))
+        # bottom left
+        new_image.alpha_composite(images[3], (0, 0))
     else:
         print("Error: Only 1-4 images can be positioned")
         return
@@ -48,6 +50,12 @@ def position_images(urls):
     # save the new image as a jpg
     #new_image.save('new_image.jpg')
 
-# example usage
-position_images(['https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0x6b175474e89094c44da98b954eedeac495271d0f.png','https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png','https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0xdac17f958d2ee523a2206206994597c13d831ec7.png'])#,'https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0x0000000000085d4780b73119b644ae5ecd22b376.png'])
+#input = ['https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0x6b175474e89094c44da98b954eedeac495271d0f.png']
+input = ['https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0x6b175474e89094c44da98b954eedeac495271d0f.png','https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png']
+#input = ['https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0x6b175474e89094c44da98b954eedeac495271d0f.png','https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png','https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0xdac17f958d2ee523a2206206994597c13d831ec7.png']
+#input = ['https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0x6b175474e89094c44da98b954eedeac495271d0f.png','https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png','https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0xdac17f958d2ee523a2206206994597c13d831ec7.png','https://upload.wikimedia.org/wikipedia/en/d/d0/Dogecoin_Logo.png']
 
+input.reverse()
+
+# example usage
+position_images(input)
